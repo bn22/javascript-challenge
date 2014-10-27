d203f3ae32ea87ad9e1aa3a95057facf897da198
unknown <bn22@uw.edu>
2014-10-26 21:50:48 -0700

    Final Version of JavaScript Challenge.

--- js/signup.js
+++ js/signup.js
@@ -4,6 +4,11 @@
 */
 "use strict";
 
+/*  onReady()
+This is called when the DOM is loaded and ready to be manipulated. This populates the state dropdown 
+menu from the us-states.js file. In addition, it also adds an event listener so the signupForm 
+can be submitted.
+*/
 function onReady() {
     var signupForm = document.getElementById('signup');
     var stateList = signupForm.elements['state'];
@@ -18,27 +23,40 @@ function onReady() {
         stateList.appendChild(option);
     }
     signupForm.addEventListener('submit', onSubmit);
-}
+} //onReady()
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('change', function(){
-    if (document.getElementById('occupation').value == 'other') {
-        document.getElementById('occupationOther').style.display = 'block';
-    } else {
-        document.getElementById('occupationOther').style.display = 'none';
-    }
-    });
-});
+/* change()
+Thks is called when the DOM is loaded and ready to be manipulated. This checks the value of the occupation form. If the value
+is equal to 'other', then a new input form should appear.
+*/
+function change() {
+   document.addEventListener('change', function(){
+        if (document.getElementById('occupation').value == 'other') {
+            document.getElementById('occupationOther').style.display = 'block';
+        } else {
+            document.getElementById('occupationOther').style.display = 'none';
+        }
+    });  
+} //change()
 
-document.addEventListener('DOMContentLoaded', function() {
-    var exit = document.getElementById('cancelButton');
+/* onExit()
+This is called when the DOM is loaded and ready to be manipulated. When the user clicks on the "No Thanks" button, a window should
+appear to ask the user if they want to leave this page. If they answer 'Ok', then it will redirect them to google.com
+*/
+function onExit() {
+   var exit = document.getElementById('cancelButton');
     exit.addEventListener('click', function () {
         if (window.confirm('Are you really sure you want to leave?')) {
             window.location = "http://www.google.com";
         }
-    });
-});
+    }); 
+} //onExit()
 
+/* onSubmit()
+This is called when the user attempts to submit the sign up form. This looks at the parameter
+passed by the browswer to see if the form is invalid. If the form is invalid, the form will not
+submit and an error message will appear.
+*/
 function onSubmit(evt) {
     try {
         var valid = validateForm(this);
@@ -56,8 +74,12 @@ function onSubmit(evt) {
     }
     evt.returnValue = valid;
     return valid;
-} 
+}  //onSubmt()
 
+/* validateForm()
+This function will attempt to validate the form by looking at its information. If the information inside the form is invalid,
+then it will return false. The parameter 'form' comes from the individual form that needs to be validated.
+*/
 function validateForm(form) {
     var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
     var idx;
@@ -67,13 +89,22 @@ function validateForm(form) {
     }
     if (document.getElementById('occupation').value == 'other') {
         var otherInput = document.getElementById('occupationOther').value;
-        if (!otherInput.trim().length > 0) {
+        otherInput = otherInput.trim();
+        if (!otherInput.length > 0) {
             document.getElementById('occupationOther').className = 'form-control invalid-field';
             valid = false;
+        } else {
+            document.getElementById('occupationOther').className = 'form-control'
         }
     }
     return valid;
-} 
+}  //validateForm()
+
+/* validateRequriedField()
+This function is called when the validateForm function reaches a field that is required. 
+This function checks if the required field by looking at its value. If the value if empty
+or doesn't match a specific required field's requirements, then it will return false.
+*/
 
 function validateRequiredField(field) {
     var value = field.value;
@@ -112,7 +143,11 @@ function validateRequiredField(field) {
         field.className = 'form-control invalid-field';
     }
     return valid;
-}
+} //validateRequiredField
 
-
+//calls the functions onReady() when the DOMContentLoaded is raised
 document.addEventListener('DOMContentLoaded', onReady);
+//calls the functions change() when the DOMContentLoaded is raised
+document.addEventListener('DOMContentLoaded', change);
+//calls the functions onExit() when the DOMContentLoaded is raised
+document.addEventListener('DOMContentLoaded', onExit);
